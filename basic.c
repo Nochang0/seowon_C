f2dc94f8c93b93c834ac04292218fe8a3f64052f
Nochang0 <hy040504@kakao.com>
2023-06-14 07:39:27 +0000

    update file

--- basic.c
+++ basic.c
@@ -1,40 +1,40 @@
 /*
-* 프로그램 내용 : 프로그래밍 6과 19번 문제
+* 프로그램 내용 : 프로그래밍 7과 11번 문제
 * 개발자 : 연승현
 * 학번 : 202311420
-* 실습일 : 2023.06.09
+* 실습일 : 2023.06.13
 */
-
 #include <stdio.h>
-#define ROWS 3
-#define COLS 3
+int main() {
+    int scores[5][4] = {
+        {28, 28, 26, 9},
+        {30, 27, 30, 10},
+        {25, 26, 24, 8},
+        {18, 22, 22, 5},
+        {24, 25, 30, 10}
+    };
+    int totals[5] = {0}; // 각 학생의 총점
+    float avg_mid = 0.0, avg_final = 0.0, avg_project = 0.0, avg_attend = 0.0; // 각 항목의 평균
 
-void add_matrix(int A[][COLS], int B[][COLS], int C[][COLS]);
-void print_matrix(char name[], int A[][COLS]);
-
-
-int main(void) {
-	int A[ROWS][COLS] = { { 2, 3, 0 }, { 8, 9, 1 }, { 7, 0, 5} };
-	int B[ROWS][COLS] = { { 1, 0, 0 }, { 1, 0, 0 }, { 1, 0, 0} };
-	int C[ROWS][COLS];
-	
-	add_matrix(A, B, C);
-	print_matrix("A", C);
-	print_matrix("B", C);
-	print_matrix("C", C);
-	return 0;
-}
-	
-int print_matrix(char name[], int A[][COLS]) {
-	int i, j;
-	
-	printf("\n ** %s matrix **\n", name);
-	for (i=0;i<ROWS;i++) {
-		for (j=0;j<COLS;j++)
-			printf("%d ", A[i][j]);
-		printf("\n")
-	}
-	return 0;
+    for (int i = 0; i < 5; i++) { // 각 학생의 총점 계산
+        for (int j = 0; j < 4; j++) {
+            totals[i] += scores[i][j];
+        }
+        printf("학생%d번: %d %d %d %d ==> %d\n", i+1, scores[i][0], scores[i][1], scores[i][2], scores[i][3], totals[i]);
+    }
+    // 각 항목의 평균 계산
+    for (int i = 0; i < 5; i++) {
+        avg_mid += scores[i][0];
+        avg_final += scores[i][1];
+        avg_project += scores[i][2];
+        avg_attend += scores[i][3];
+    }
+    avg_mid /= 5;
+    avg_final /= 5;
+    avg_project /= 5;
+    avg_attend /= 5;
+    printf("항목별 평균: %.2f %.2f %.2f %.2f\n", avg_mid, avg_final, avg_project, avg_attend);
+    return 0;
 }
 		 
 // gcc basic.c -o basic.out -lm && ./basic.out
