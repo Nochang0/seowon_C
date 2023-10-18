4c60a22619494ad3e3c054abeb960d91a8e7efff
root <hy040504@kakao.com>
2023-10-18 03:40:19 +0000

    make it better

--- semester2/c_12chapter/2023.10.18/filess.c
+++ semester2/c_12chapter/2023.10.18/filess.c
@@ -1,3 +1,10 @@
+/*
+* 프로그램 내용 : int 배열을 2진 파일로 저장하고 읽어 오기
+* 개발자 : 연승현
+* 학번 : 202311420
+* 실습일 : 2023.10.18
+*/
+
 #include <stdio.h>    // 입출력 관련 함수를 사용하기 위한 헤더 파일
 #include <stdlib.h>   // 일반적인 유틸리티 함수와 동적 메모리 할당을 위한 헤더 파일
 #include <string.h>   // 문자열 처리를 위한 헤더 파일
@@ -26,11 +33,12 @@ int main(void) {
 cleanup:
     if (fin == NULL || fout == NULL)
         printf("\n");
-    else if (ferror(fin))
+    else if (ferror(fin));
         printf("파일 읽기 실패\n");
     else if (ferror(fout))
-        else printf("파일 쓰기 실패\n");
-    printf(" ");
+    	printf("파일 쓰기 실패\n");
+    else 
+		printf("파일 복사 성공\n");
     if (fin) fclose(fin);
     if (fout) fclose(fout);
     return 0;
